ed745e162bca9e9dd8eeb047becfbf32dad8bfa0
JO <zambranac143@gmail.com>
2023-10-31 16:18:13 -0400

    Create class for hw map + add drive motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class HardwareMap {
+    //Create Drive Motors
+    public DcMotor frontRightMotor = null;
+    public DcMotor frontLeftMotor = null;
+    public DcMotor backRightMotor = null;
+    public DcMotor backLeftMotor = null;
+}
