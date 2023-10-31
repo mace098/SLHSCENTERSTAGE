e907b05c971068c27dde6bd889335952fb246be9
liamkelly <liamxbk@gmail.com>
2023-10-31 16:52:11 -0400

    Add all the other motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -8,4 +8,16 @@ public class HardwareMap {
     public DcMotor frontLeftMotor = null;
     public DcMotor backRightMotor = null;
     public DcMotor backLeftMotor = null;
+
+    //Create weed wacker motor
+    public DcMotor weedWackerMotor = null;
+
+    //Create lifting and wheel motor
+    public DcMotor liftWheelMotor = null;
+
+    //Create belt motor
+    public DcMotor beltMotor = null;
+
+    //Create bench press motor
+    public DcMotor benchPressMotor = null;
 }
