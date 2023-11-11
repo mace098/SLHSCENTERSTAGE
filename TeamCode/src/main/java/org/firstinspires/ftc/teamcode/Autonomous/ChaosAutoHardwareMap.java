60f7a940b6fa9ef0056c79b1e51e65ebbf1ef6a9
SevenBitScience <milausnic@gmail.com>
2023-11-11 15:09:38 -0500

    Move & rename AutoHwMap. Also updated comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ChaosAutoHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ChaosAutoHardwareMap.java
@@ -1,40 +1,40 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-// based on code provided by Fire Robotics
-public class HardwareMap {
-    //Create Drive Motors
+// Based on code provided by Fire Robotics
+public class ChaosAutoHardwareMap {
+    // Create Drive Motors
     public DcMotor frontRightMotor = null;
     public DcMotor frontLeftMotor = null;
     public DcMotor backRightMotor = null;
     public DcMotor backLeftMotor = null;
 
-    //Create weed wacker motor
+    // Create weed wacker motor
     public DcMotor weedWackerMotor = null;
 
-    //Create lifting and wheel motor
+    // Create lifting and wheel motor
     public DcMotor liftWheelMotor = null;
 
-    //Create belt motor
+    // Create belt motor
     public DcMotor beltMotor = null;
 
-    //Create bench press motor
+    // Create bench press motor
     public DcMotor benchPressMotor = null;
 
     com.qualcomm.robotcore.hardware.HardwareMap hardwareMap = null;
     public ElapsedTime runtime = new ElapsedTime();
 
-    public HardwareMap(com.qualcomm.robotcore.hardware.HardwareMap hwMap) {
+    public ChaosAutoHardwareMap(com.qualcomm.robotcore.hardware.HardwareMap hwMap) {
         init(hwMap);
     }
 
+    // Initialize devices
     private void init(com.qualcomm.robotcore.hardware.HardwareMap hwMap) {
         hardwareMap = hwMap;
 
-        // movement (wheel) motors connection
+        // Drive motors connection
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
@@ -46,7 +46,7 @@ public class HardwareMap {
         beltMotor = hardwareMap.get(DcMotor.class, "beltMotor");
         benchPressMotor = hardwareMap.get(DcMotor.class, "benchPressMotor");
 
-        // motor directions; subject to change?
+        // Motor directions; subject to change
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backRightMotor.setDirection(DcMotor.Direction.FORWARD);
@@ -57,39 +57,44 @@ public class HardwareMap {
         beltMotor.setDirection(DcMotor.Direction.FORWARD);
         benchPressMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        // set motor mode
+        // Set the modes for the motors
+
+        // First setting up the drive motors
+        // Starting by resetting the encoders
         frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        // And then set them to run to position mode
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // likewise for those function motors
+        // Likewise for those function motors
+        // Reset encoders
         liftWheelMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         weedWackerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         beltMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         benchPressMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+        // These are TBD
         liftWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         weedWackerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         beltMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         benchPressMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // set zero power behavior
+        // Set all motors to break when power = 0
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        // these ones below likely subject to change
+        // These ones might change later
         liftWheelMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         weedWackerMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         beltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         benchPressMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // set motors to use no power upon init
+        // Set all motors to brake upon init
         frontRightMotor.setPower(0.0);
         frontLeftMotor.setPower(0.0);
         backRightMotor.setPower(0.0);
