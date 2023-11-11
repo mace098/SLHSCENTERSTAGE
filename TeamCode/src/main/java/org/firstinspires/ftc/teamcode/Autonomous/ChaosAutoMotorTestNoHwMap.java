268bac99f0af17b276f3f25e3d1f5a5678681d88
SevenBitScience <milausnic@gmail.com>
2023-11-11 15:54:16 -0500

    Clean up and add more motors to AutoMotorTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ChaosAutoMotorTestNoHwMap.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "ChaosAutoMotorTest_A")
+public class ChaosAutoMotorTestNoHwMap extends LinearOpMode {
+    DcMotor frontRightMotor = null;
+    DcMotor frontLeftMotor = null;
+    public DcMotor backRightMotor = null;
+    public DcMotor backLeftMotor = null;
+
+    @Override
+    public void runOpMode() {
+        //int ticks_per_rev = 1400;
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+
+        // Run motor setup
+        SetupMotors();
+
+        waitForStart();
+
+        // Some demo moves
+        Drive(1.0, 100);
+        Drive(-1.0, 1000);
+        Drive(0.5,100);
+    }
+
+    public void SetupMotors() {
+        // Set motor directions
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // Reset encoders
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Set mode and brake motors on power = 0
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void Drive(double power, int position) {
+        // Set the target position for motors
+        frontRightMotor.setTargetPosition(position);
+        frontLeftMotor.setTargetPosition(position);
+        backRightMotor.setTargetPosition(position);
+        backLeftMotor.setTargetPosition(position);
+
+        // Power on the motors
+        frontRightMotor.setPower(power);
+        frontLeftMotor.setPower(power);
+        backRightMotor.setPower(power);
+        backLeftMotor.setPower(power);
+
+        // Wait for the motors to finish moving
+        while (frontRightMotor.isBusy()) {}
+    }
+
+    public void BrakeMotors() {
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+    }
+}
